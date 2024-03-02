d156dca793bc4b7019de83896809fb2dab452686
Phil Cooper <philjcooper@outlook.com>
2024-03-02 14:03:22 +0000

    added initial setup files and started on head section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Header from './components/Header/Header.jsx';
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
