fc16d8166bf3bcc130be349a2bedebb7e536ada5
Phil Cooper <philjcooper@outlook.com>
2024-03-06 21:55:32 +0000

    update cv link

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
             <a href="https://www.linkedin.com/in/philjcooper/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
             <a href="https://github.com/PhilC7" target="_blank"><i className="fa-brands fa-square-github"></i></a>
             <a
-              href="public/phil-cooper-cv.pdf"
+              href="src/assets/phil-cooper-cv.pdf"
               download="phil-cooper-cv.pdf"
               className="icon-link">
               <i className="fa-solid fa-file"></i><span className="overlay-text">CV</span>
